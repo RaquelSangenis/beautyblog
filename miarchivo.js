2c025da1269314e55521d899f55c4aef974984a4
Raquel Sangenis <licsangenis@gmail.com>
2022-08-09 12:45:11 -0300

    correcciones

--- miarchivo.js
+++ miarchivo.js
@@ -1,5 +1,4 @@
-alert ("Hi Beauty! Bienvenida a Todo Beauty!")
-
+alert("Hi Beauty! Bienvenida a Todo Beauty!");
 
 /*function horasSueno(){
     let horasSueno = prompt ("Cuantas horas duermes al dia?")
@@ -26,51 +25,55 @@ alert ("Hi Beauty! Bienvenida a Todo Beauty!")
 
 horasSueno()*/
 
-function tipoPiel(){
-    let tipoPiel = prompt ("Cual es tu tipo de piel? \n normal \n mixta \n grasa \n sensible").toLowerCase()
+function tipoPiel() {
+  let tipoPiel = prompt(
+    "Cual es tu tipo de piel? \n normal \n mixta \n grasa \n sensible"
+  ).toLowerCase();
 
-    switch (tipoPiel){
-        case "normal":
-            alert("Tienes muchos productos para elegir")
-            break
-        case "mixta":
-            alert("Hay varios productos para tu tipo de piel")
-            break
-        case "grasa":
-            alert("Deberias elegir productos ligeros y sin aceites")
-            break
-        case "sensible":
-            alert("Deberias elegir productos libres de alcohol y fragancia")
-            break
-        default:
-        alert("Debes identificar tu tipo de piel")
-        break
-    }
+  switch (tipoPiel) {
+    case "normal":
+      alert("Tienes muchos productos para elegir");
+      break;
+    case "mixta":
+      alert("Hay varios productos para tu tipo de piel");
+      break;
+    case "grasa":
+      alert("Deberias elegir productos ligeros y sin aceites");
+      break;
+    case "sensible":
+      alert("Deberias elegir productos libres de alcohol y fragancia");
+      break;
+    default:
+      alert("Debes identificar tu tipo de piel");
+      break;
+  }
 }
 
-tipoPiel()
+//tipoPiel();
 
-
-class Product{
-    constructor(name, price, image){
-        this.name = name;
-        this.price = price;
-        this.image = image;
-    }
+class Producto {
+  constructor(nombre, precio, img) {
+    this.name = nombre;
+    this.price = precio;
+    this.image = img;
+  }
 }
 
-const prod1 = new Product("Agua micelar", 1800 , "img1");
-const prod2 = new Product("Limpiador", 1500, "img2" );
-const prod3 = new Product("Exfoliante", 1400, "img3" );
-const prod4 = new Product("Contorno ojos", 1400, "img4" );
-const prod5 = new Product("Tonico", 1000, "img5" );
-const prod6 = new Product("Escencia", 1200, "img6" );
-const prod7 = new Product("Serum", 1800, "img7" );
-const prod8 = new Product("Hidratante", 1400, "img8");
-const prod9 = new Product("Protector solar",1200, "img9");
-const prod10 = new Product("Balsamo labial", 1400, "img10");
-    
-let products = [prod1,prod2,prod3,prod4,prod5,prod6,prod7,prod8,prod9,prod10];
+const productos = [];
+function agregarProductos() {
+  productos.push(new Producto("Agua micelar", 1200, "img1"));
+  productos.push(new Producto("Limpiador", 1400, "img2"));
+  productos.push(new Producto("Exfoliante", 1400, "img3"));
+  productos.push(new Producto("Contorno ojos", 1400, "img4"));
+  productos.push(new Producto("Tonico", 1000, "img5"));
+  productos.push(new Producto("Escencia", 1200, "img6"));
+  productos.push(new Producto("Serum", 1800, "img7"));
+  productos.push(new Producto("Hidratante", 1400, "img8"));
+  productos.push(new Producto("Protector solar", 1200, "img9"));
+  productos.push(new Producto("Balsamo labial", 1400, "img10"));
 
-console.log("Productos disponibles");
-console.log(products);
+  console.log("Productos disponibles");
+  console.log(productos);
+}
+
+agregarProductos();
