f7679c09418ac3f13cff23ed862de0556de54f41
Raquel Sangenis <licsangenis@gmail.com>
2022-08-18 19:50:39 -0300

    desafio utilizando DOM y eventos

--- miarchivo.js
+++ miarchivo.js
@@ -1,149 +1,123 @@
-alert("Hi Beauty! Bienvenida a Todo Beauty!");
-debugger
 class Producto {
-  constructor(id, nombre, precio, img) {
+  constructor(id, nombre, precio, imagen) {
     this.id = id;
     this.nombre = nombre;
     this.precio = precio;
-    this.imagen = img;
+    this.imagen = imagen;
   }
 }
 
+let productos = [];
 
-const productos = [];
-
-function mostrarProductos() {
-  productos.push(new Producto(1,"Agua micelar", 1200, "img1"));
-  productos.push(new Producto(2, "Limpiador", 1400, "img2"));
-  productos.push(new Producto(3,"Exfoliante", 1400, "img3"));
-  productos.push(new Producto(4, "Contorno ojos", 1400, "img4"));
-  productos.push(new Producto(5, "Tonico", 1000, "img5"));
-  productos.push(new Producto(6, "Escencia", 1200, "img6"));
-  productos.push(new Producto(7, "Serum", 1800, "img7"));
-  productos.push(new Producto(8, "Hidratante", 1400, "img8"));
-  productos.push(new Producto(9, "Protector solar", 1200, "img9"));
-  productos.push(new Producto(10, "Balsamo labial", 1400, "img10"));
+function cargarProductos() {
+  productos.push(new Producto(1, "Agua micelar", 1200, "images/amicelar.jpg"));
+  productos.push(new Producto(2, "Limpiador", 1400, "images/limpieza.jpg"));
+  productos.push(new Producto(3, "Exfoliante", 1400, "images/exfoliante.jpg"));
+  productos.push(new Producto(4, "Contorno ojos", 1400, "images/contorno.jpg"));
+  productos.push(new Producto(5, "Tonico", 1000, "images/niacinamida.jpg"));
+  productos.push(new Producto(6, "Escencia", 1200, "images/niacinamida.jpg"));
+  productos.push(new Producto(7, "Serum", 1800, "images/hidratante.jpg"));
+  productos.push(new Producto(8, "Hidratante", 1400, "images/hidratante.jpg"));
+  productos.push(
+    new Producto(9, "Protector solar", 1200, "images/pantalla solar.jpg")
+  );
+  productos.push(new Producto(10, "Balsamo labial", 1400, "images/labial.jpg"));
 }
 
-mostrarProductos();
+cargarProductos();
 
-
-let respuesta = confirm("Desea ver algun producto en especifico?");
-if (respuesta == 1) {
-  let nombreProducto = elegirProducto();
-  let productosFiltrados = filtrarProductos(nombreProducto);
-  console.log("Estos son los productos que quizas estes buscando")
-  console.log(productosFiltrados)
-}else{
-  console.log("Ok, aqui tienes para elegir entre todos los productos que tenemos disponibles.")
-  console.log(productos)
+function renderizarProductos() {
+  productos.forEach(function (prod) {
+    document.querySelector("#productos").insertAdjacentHTML(
+      "beforeend",
+      `
+        <div class="card" style="width: 18rem">
+          <img
+            src="${prod.imagen}"
+            class="card-img-top"
+            alt="${prod.nombre}"
+          />
+          <div class="card-body">
+            <h5 class="card-title">${prod.nombre}</h5>
+            <p class="card-text">$${prod.precio}</p>
+            <a href="#" id="producto${prod.id}" class="btn btn-primary">Comprar</a>
+          </div>
+        </div>
+         `
+    );
+  });
 }
+renderizarProductos();
 
-
-function elegirProducto() {
-  let nombreProducto = prompt(
-    "Genial, estas en el lugar indicado. Por favor ingresa el nombre del producto.");
-  while (nombreProducto.trim() == "") {
-    nombreProducto = prompt("Por favor ingresa un nombre de producto valido.");
+document.addEventListener("keyup", function (e) {
+  let palabra = e.target.value;
+  if (palabra.trim() == "") {
+    productos = [];
+    cargarProductos();
+    renderizarProductos();
+  } else {
+    filtrarProductos();
   }
-  return nombreProducto.toLowerCase();
+});
+
+function buscarProductoXID(id) {
+  return productos.find((prod) => prod.id === id);
 }
 
-elegirProducto();
+function vaciarListadoProductos() {
+  document.getElementById("productos").innerHTML = "";
+}
 
-function filtrarProductos(nombre) {
-  let filtrados = productos.filter(function(prod){
-    return prod.nombre.toLowerCase() == nombre
+function filtrarProductos() {
+  console.log("filtrarProductos");
+  let nombre = document.getElementById("buscador").value.toLowerCase();
+  if (nombre.trim() === "") {
+    document.getElementById("warning").innerHTML =
+      "Por favor ingrese un nombre de producto valido";
+    return;
+  }
+
+  let filtrados = productos.filter(function (prod) {
+    return prod.nombre.toLowerCase() == nombre;
   });
 
-  return filtrados;
+  if (filtrados.length > 0) {
+    productos = filtrados;
+    vaciarListadoProductos();
+    renderizarProductos();
+  }
 }
 
+let carrito = [];
 
-let carrito = []; 
-
-function agregarCarrito(id){
-  let productoElegido = productos.find(function(prod){
-    return prod.id === id
+function agregarAlCarrito(idProd) {
+  let productoElegido = productos.find(function (prod) {
+    return prod.id === idProd;
   });
 
-  carrito.push(productoElegido);
-  console.log("Tu carrito ahora tiene estos productos");
-  console.log(carrito);
-
+  if (carrito.some((producto) => producto.id == idProd)) {
+    let prod = carrito.find((producto) => producto.id == idProd);
+    prod.cantidad++;
+  } else {
+    productoElegido.cantidad = 1;
+    carrito.push(productoElegido);
+  }
 }
 
-agregarCarrito(4);
-agregarCarrito(7);
+agregarAlCarrito(4);
+agregarAlCarrito(7);
+agregarAlCarrito(4);
 
-
-function sacarCarrito(id){
-    const resultado = carrito.some(producto => producto.id === id)
-    if (resultado){
-      
+function eliminarDelCarrito(idProd) {
+  let prod = carrito.find((producto) => producto.id === idProd);
+  if (prod) {
+    if(prod.cantidad > 1){
+        prod.cantidad--
+    }else{
+      let posicion = carrito.findIndex(p => p.id == idProd)
+      if (posicion != -1){
+       carrito.splice(posicion,1)
+      }
     }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function tipoPiel() {
-  let tipoPiel = prompt(
-    "Cual es tu tipo de piel? \n normal \n mixta \n grasa \n sensible"
-  ).toLowerCase();
-
-  switch (tipoPiel) {
-    case "normal":
-      alert("Tienes muchos productos para elegir");
-      break;
-    case "mixta":
-      alert("Hay varios productos para tu tipo de piel");
-      break;
-    case "grasa":
-      alert("Deberias elegir productos ligeros y sin aceites");
-      break;
-    case "sensible":
-      alert("Deberias elegir productos libres de alcohol y fragancia");
-      break;
-    default:
-      alert("Debes identificar tu tipo de piel");
-      break;
   }
 }
-
-//tipoPiel();
